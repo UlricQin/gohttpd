05db8e0ce5f4e6e5659765c7f88351bab728ad65
Ulric Qin <ulric.qin@gmail.com>
2024-04-10 09:05:18 +0800

    add .gitignore

--- build.sh
+++ build.sh
@@ -2,5 +2,3 @@
 
 GOOS=linux GOARCH=amd64 go build -o gohttpd-linux-amd64 main.go
 GOOS=linux GOARCH=arm64 go build -o gohttpd-linux-arm64 main.go
-GOOS=windows GOARCH=amd64 go build -o gohttpd-windows-amd64 main.go
-GOOS=windows GOARCH=arm64 go build -o gohttpd-windows-arm64 main.go
